40f08764eea410f618c534bd40806aabdc92c19d
shivani-cell <shimittal@microsoft.com>
2022-12-25 12:43:03 +0530

    user schema works fine

---
+++ backend/models/doctorSchema.js
@@ -0,0 +1,22 @@
+const mongoose=require("mongoose")
+const { stringify } = require("nodemon/lib/utils")
+
+const addressSchema=new mongoose.Schema({
+   country: String,
+   state: String,
+   city: String,
+   pincode: String, 
+})
+
+const UserDetailsSchema=new mongoose.Schema({
+name: String,
+email: String,
+password: String,
+phoneno: String,
+isDoctor: Boolean,
+address: [addressSchema],
+},{
+collection: "Userinfo",
+});
+
+mongoose.model("userDetails",UserDetailsSchema);
