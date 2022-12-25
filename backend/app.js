4f448c323b8bfbdd06e3f25aeea7b42ac7d6c6c4
shivani-cell <shimittal@microsoft.com>
2022-12-25 12:42:30 +0530

    user schema works fine

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@ const express=require("express")
 const app=express()
 const mongoose=require("mongoose")
 app.use(express.json());
-require("./userDetails")
+require("./models/userDetails")
 
 const mongourl="mongodb+srv://shimittal:shivani@cluster0.s51nxga.mongodb.net/?retryWrites=true&w=majority";
 mongoose.connect(mongourl,{
@@ -11,30 +11,26 @@ mongoose.connect(mongourl,{
     console.log("connected to database");
 }).catch((e)=>console.log(e))
 
-const user=mongoose.model("PatientDetails");
+const user=mongoose.model("userDetails");
 
 app.listen(5000,()=>{
     console.log("server started")
 })
 
-app.post("/post",async(req,res)=>{
-    console.log(req.body);
-    const {data}=req.body;
-    if(data=="shivani")
-    {
-        res.send({status:"ok"})
-    }
-})
+
 
 app.post("/register",async(req,res)=>{
     console.log("Hi")
-    const {name,email,password,phoneno}=req.body;
+    console.log(req.body)
+    const {name,email,password,phoneno,address}=req.body;
     try{
         await user.create({
             name:name,
             email:email,
             password:password,
             phoneno:phoneno,
+            isDoctor:false,
+            address:req.body.address
         });
         res.send({status:"ok"})
     }
