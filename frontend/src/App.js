48bd15f831569123faab6617bdfa7c2fcfb315ad
sodhiman <soni.dhiman@oracle.com>
2023-01-03 20:04:55 +0530

    registercode

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,30 @@
-import React,{component} from 'react';
-import './App.css';
-import {BrowserRouter,Route,Switch} from 'react-router-dom';
-import Login from "./component/Login/Login";
-import Register from "./component/Register/Register";
+import './App.css'
+import Homepage from "./components/homepage/homepage"
+import Login from "./components/login/login"
+import Register from "./components/register/register"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useState } from 'react';
+
 function App() {
+
+  const [ user, setLoginUser] = useState({})
   return (
-   <div>
-        <BrowserRouter>
-          <Switch>
-            <Route exact path="/" component={Register} />
-          </Switch>
-        </BrowserRouter>
-      </div>
-      
-    );
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route exact path="/" element= {user && user._id ? <Homepage setLoginUser={setLoginUser} /> : <Login setLoginUser={setLoginUser}/>}>
+    
+          </Route>
+          <Route path="/login"  element= {<Login/>}>
+          
+          </Route>
+          <Route path="/register" element={<Register/>}>
+  
+          </Route>
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
